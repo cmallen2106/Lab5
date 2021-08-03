273610551a25e10874f3442cdb554b7fc4853da6
cmallen2106 <cmallen2106@gmail.com>
2021-08-03 07:13:00 -0600

    import packages and create new file

--- lab5Ex1/src/lab5Ex1/Input.java
+++ lab5Ex1/src/lab5Ex1/Input.java
@@ -1,9 +1,20 @@
+//Christina Sosa
+//Programming Fundamentals 
+//Summer 2021
+
 package lab5Ex1;
+import java.io.FileNotFoundException; 
+import java.io.File;
+import java.util.Scanner;
+
+
 
 public class Input {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File f = new File ("/Users/christina/Desktop/Input.csv"); 
+		Scanner file = new Scanner(f); 
 
 	}
 
