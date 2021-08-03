f8ee60898f7f0944bdb8808122bca227054690de
cmallen2106 <cmallen2106@gmail.com>
2021-08-04 00:25:48 +0100

    Initial commit

--- lab5Ex1/src/lab5Ex1/Input.java
+++ lab5Ex1/src/lab5Ex1/Input.java
@@ -11,10 +11,33 @@ import java.util.Scanner;
 
 public class Input {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String args[]) throws FileNotFoundException {
 		
-		File f = new File ("/Users/christina/Desktop/Input.csv"); 
-		Scanner file = new Scanner(f); 
+		Scanner scan = new Scanner(new File("/Users/christina/Desktop/Input.csv")); 
+		String line = ""; 
+		
+		int max = 0, count = 1; 
+		String[] arr = null; 
+		
+		System.out.println("Maximum values"); 
+		
+		while(scan.hasNext()) {
+			 
+			line = scan.nextLine(); 
+			arr = line.split(","); 
+			max = 0;
+			
+			for(int i=0; i<arr.length; i++) {
+				
+				if(Integer.parseInt(arr[i]) > max)
+					max = Integer.parseInt(arr[i]); 
+				
+			}
+			
+			System.out.println("ROW " + count + " : " + max); 
+			count++;
+			
+		}
 		
 		
 
---
+++ lab5Ex2/src/lab5Ex2/Output.java
@@ -0,0 +1,10 @@
+package lab5Ex2;
+
+public class Output {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
