91ba8fec9e8ea9e639bc28680cb0a75dc174de2a
cmallen2106 <cmallen2106@gmail.com>
2021-08-04 15:50:57 +0100

    enter a string

--- lab5Ex4/src/lab5Ex4/StringReversal.java
+++ lab5Ex4/src/lab5Ex4/StringReversal.java
@@ -3,10 +3,17 @@
 //Lab 5 Exercise 4
 
 package lab5Ex4;
+import java.util.Scanner; 
 
 public class StringReversal {
 
 	public static void main(String[] args) {
+		
+		StringReversal s = new StringReversal(); 
+		Scanner scan = new Scanner(System.in); 
+		
+		System.out.println("Enter a string: "); 
+		
 		
 		
 
