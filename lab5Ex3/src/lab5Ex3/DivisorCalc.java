4516e7ea29c8170a87cdd7eb411847cca3d73d1c
cmallen2106 <cmallen2106@gmail.com>
2021-08-04 15:18:39 +0100

    changed program to correct java project and class

--- lab5Ex3/src/lab5Ex3/DivisorCalc.java
+++ lab5Ex3/src/lab5Ex3/DivisorCalc.java
@@ -3,11 +3,45 @@
 //Lab 5 Exercise 3
 
 package lab5Ex3;
+import java.util.Scanner; 
 
 public class DivisorCalc {
+	
+	public static int gcd (int num1, int num2) {
+		
+		if (num1 < num2) {
+			int temp = num1; 
+			num1 = num2; 
+			num2 = temp; 
+		} 
+		
+		if (num1 % num2 == 0) {
+			return num2; 
+		}
+		
+		int remainder = num1 % num2; 
+		
+		return gcd(num2, remainder); 
+	}
 
 	public static void main(String[] args) {
 		
+		//Asking user for two integers and scanning numbers into num1 and num2
+		
+		Scanner scan = new Scanner (System.in);
+		
+		int num1, num2; 
+		
+		System.out.println("Enter two integers: "); 
+		System.out.println("First Number: "); 
+		num1 = scan.nextInt(); 
+		
+		System.out.println("Second Number: ");
+		num2 = scan.nextInt(); 
+		
+		System.out.println("Greatest Common Divisor: " + gcd(num1, num2)); 
+		
+		scan.close();
 
 	}
 
--- lab5Ex4/src/lab5Ex4/StringReversal.java
+++ lab5Ex4/src/lab5Ex4/StringReversal.java
@@ -3,28 +3,12 @@
 //Lab 5 Exercise 4
 
 package lab5Ex4;
-import java.util.Scanner; 
 
 public class StringReversal {
 
 	public static void main(String[] args) {
 		
-		//Asking user for two integers and scanning numbers into num1 and num2
 		
-		Scanner scan = new Scanner (System.in);
-		
-		int num1, num2; 
-		
-		System.out.println("Enter two integers: "); 
-		System.out.println("First Number: "); 
-		num1 = scan.nextInt(); 
-		
-		System.out.println("Second Number: ");
-		num2 = scan.nextInt(); 
-		
-		System.out.println(" "); 
-		
-		scan.close();
 
 	}
 
