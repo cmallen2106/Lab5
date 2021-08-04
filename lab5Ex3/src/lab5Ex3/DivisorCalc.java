6f872099d58f5ccfb665178a73b02337bfd70a2e
cmallen2106 <cmallen2106@gmail.com>
2021-08-04 14:23:30 +0100

    Initial Commit

---
+++ lab5Ex3/src/lab5Ex3/DivisorCalc.java
@@ -0,0 +1,14 @@
+//Christina Sosa
+//Programming Fundamentals - Summer 2021
+//Lab 5 Exercise 3
+
+package lab5Ex3;
+
+public class DivisorCalc {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
